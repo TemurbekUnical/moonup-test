925d66125473c9800b4c56fa541592ca328e54a2
Temurbek <temurbekunical@gmail.com>
2024-04-27 19:11:58 +0500

    ok

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,18 @@
+"use client";
 import DashboardNavbar from "@/components/shared/navbar";
+import { isLoggedIn } from "@/services/auth";
 import { LayoutProps } from "@/types/app.types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      router.replace("/login");
+    }
+  }, []);
+
   return (
     <div>
       <DashboardNavbar />
